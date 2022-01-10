d85101570c901cdfdd0890fafb5c31d7d39cd5f6
snit29 <95476859+snit29@users.noreply.github.com>
2022-01-10 11:35:27 +0100

    Added Files

---
+++ Web_I-O_Nitsche_Weismann/Web_I-O_Nitsche_Weismann/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Web_I_O_Nitsche_Weismann
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine();
+        }
+    }
+}
